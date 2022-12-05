f08504c31c01133bd26cabaa813558f8b33e2fec
giulio <turrisigiulio@gmail.com>
2022-12-05 17:24:11 +0100

    creating interface controller acados

---
+++ python_scripts/controllers/acados/acados_nmpc.py
@@ -0,0 +1,118 @@
+from acados_template import AcadosOcp, AcadosOcpSolver, AcadosSimSolver
+from acados_model import export_robot_model
+import numpy as np
+import scipy.linalg
+
+import time
+
+import sys
+sys.path.append('/home/python_scripts/')
+import euler_integration
+from twip_dynamics import Twip_dynamics
+
+
+class NMPC:
+    def __init__(self, horizon, dt):
+        self.horizon = horizon  # Define the number of discretization steps
+        self.dt = dt
+
+        self.T_horizon = self.horizon*self.dt
+
+        self.twip = Twip_dynamics()
+
+        self.ocp = self.create_ocp_solver_description()
+        self.acados_ocp_solver = AcadosOcpSolver(
+            self.ocp, json_file="acados_nmpc_" + self.ocp.model.name + ".json"
+        )
+
+        self.state_dim = self.ocp.model.x.size()[0]
+        self.control_dim = self.ocp.model.u.size()[0]
+
+    def create_ocp_solver_description(self,) -> AcadosOcp:
+        # create ocp object to formulate the OCP
+        ocp = AcadosOcp()
+
+        model = export_robot_model()
+        ocp.model = model
+        nx = model.x.size()[0]
+        nu = model.u.size()[0]
+        ny = nx + nu
+
+        # set dimensions
+        ocp.dims.N = self.horizon
+
+        # set cost
+        Q_mat = 2 * np.diag([0, 5, 0.0, 2, 1, 0.2])  # [x,y,x_d,y_d,th,th_d]
+        R_mat = 2 * 5 * np.diag([1, 1])
+
+        ocp.cost.cost_type = "LINEAR_LS"
+        ocp.cost.cost_type_e = "LINEAR_LS"
+
+        ny = nx + nu
+        ny_e = nx
+
+        ocp.cost.W_e = Q_mat
+        ocp.cost.W = scipy.linalg.block_diag(Q_mat, R_mat)
+
+        ocp.cost.Vx = np.zeros((ny, nx))
+        ocp.cost.Vx[:nx, :nx] = np.eye(nx)
+
+        Vu = np.zeros((ny, nu))
+        Vu[nx : nx + nu, 0:nu] = np.eye(nu)
+        ocp.cost.Vu = Vu
+
+        ocp.cost.Vx_e = np.eye(nx)
+
+        ocp.cost.yref = np.zeros((ny,))
+        ocp.cost.yref_e = np.zeros((ny_e,))
+
+        # set constraints
+        #ocp.constraints.lbu = np.array([-F_max])
+        #ocp.constraints.ubu = np.array([+F_max])
+        #ocp.constraints.idxbu = np.array([0])
+
+        #ocp.constraints.x0 = X0
+
+        # set options
+        ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"  # FULL_CONDENSING_QPOASES
+        ocp.solver_options.hessian_approx = "GAUSS_NEWTON"  # 'GAUSS_NEWTON', 'EXACT'
+        ocp.solver_options.integrator_type = "IRK"
+        ocp.solver_options.nlp_solver_type = "SQP_RTI"  # SQP_RTI, SQP
+        ocp.solver_options.nlp_solver_max_iter = 400
+        # ocp.solver_options.levenberg_marquardt = 1e-2
+
+        # set prediction horizon
+        ocp.solver_options.tf = self.T_horizon
+
+        return ocp
+
+    def compute_control(self, state, state_des):
+
+        # initialize solver
+        for stage in range(self.horizon + 1):
+            self.acados_ocp_solver.set(stage, "x", 0.0 * np.ones((self.state_dim,)))
+        for stage in range(self.horizon):
+            self.acados_ocp_solver.set(stage, "u", np.zeros((self.control_dim,)))
+
+        for j in range(self.horizon):
+            yref = np.array([state_des, 0, 0])
+            self.acados_ocp_solver.set(j, "yref", yref)
+        yref_N = np.array(state_des)
+        self.acados_ocp_solver.set(self.horizon, "yref", yref_N)
+
+
+        start_time = time.time()
+        # set initial state constraint
+        self.acados_ocp_solver.set(0, "lbx", state)
+        self.acados_ocp_solver.set(0, "ubx", state)
+
+
+        # solve ocp
+        status = self.acados_ocp_solver.solve()
+
+        control = self.acados_ocp_solver.get(0, "u")
+
+
+        print("control time: ", time.time()-start_time)
+
+        return control
