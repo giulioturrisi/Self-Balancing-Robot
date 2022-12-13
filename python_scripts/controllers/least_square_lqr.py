72434f86ad83d07a241e3e61e866c40bb40fb195
giulio <turrisigiulio@gmail.com>
2022-12-13 23:06:12 +0100

    added recursive least square

--- python_scripts/controllers/least_square_lqr.py
+++ python_scripts/controllers/least_square_lqr.py
@@ -5,11 +5,25 @@ sys.path.append('/home/python_scripts/')
 import euler_integration
 from twip_dynamics import Twip_dynamics
 
-class LQR:
+
+
+class LQR_LS:
+    """This is a small class that computes an LQR control law based on a least square update."""
+    
     def __init__(self, lin_state = None, lin_tau = None, horizon = None, dt = None):
+        """
+        Args:
+            lin_state (np.array): linearization state
+            lin_tau (np.array): linearization control inputs
+            horizon (int): how much to look into the future for optimizing the gains 
+            dt (int): desidered sampling time
+        """
         self.lin_state = np.zeros(6)
         self.lin_tau = np.zeros(2)
-        self.horizon = 2000
+        if(horizon==None):
+            self.horizon = 2000
+        else:
+            self.horizon = horizon
         self.dt = dt
 
         self.twip = Twip_dynamics()
@@ -34,8 +48,20 @@ class LQR:
         self.best_param = None
 
 
-    def calculate_discrete_LQR_gain(self,lin_state, lin_tau):
+        self.P_least_square = None
+        
 
+
+    def calculate_discrete_LQR_gain(self,lin_state, lin_tau):
+        """Calculate by backward iterations the optimal LQR gains
+
+        Args:
+            lin_state (np.array): linearization state
+            lin_tau (np.array): linearization control inputs
+
+        Returns:
+             K (np.array): optimal gains
+        """
         P_next = np.identity(6)
         P_next[0,0] = 0.0 #x
         P_next[2,2] = 0.0 #yaw
@@ -46,7 +72,6 @@ class LQR:
         A_discrete = A*self.dt + np.identity(6)
         B_discrete = B*self.dt
 
-
         for i in range(0, self.horizon):
             Q_uu = self.R + B_discrete.T@P_next@B_discrete
 
@@ -55,46 +80,104 @@ class LQR:
             
             self.K = (np.linalg.pinv(self.R + B_discrete.T@P_next@B_discrete)@B_discrete.T@P_next@A_discrete)
 
-        print("K discrete", self.K)
         return self.K
 
+
     def lift_space(self, state):
+        """Nonlinear features for least square
+
+        Args:
+            state (np.array): state to lift 
+
+        Returns:
+            (np.array): nonlinear state
+        """
         return np.array([1, state[1]*state[1], state[2]*state[2], state[3]*state[3], state[4]*state[4], state[5]*state[5]])
 
 
-    def update_dataset(self, previous_state, control, state_meas):
+    def compute_error_and_lift(self, previous_state, control, state_meas):
+        """compute error signal and lift the state
+
+        Args:
+            previous_state (np.array): state at timestep K-1
+            control (np.array): control at timestep K-1
+            state_meas (np.array): state measured at time K  
+
+        Returns:
+            (np.array, np.array): nonlinear state, error signal
+        """
         next_state = self.twip.forward_dynamics(previous_state, control)
         qdd = next_state[3:6]
         state_pred = euler_integration.euler_integration(previous_state, qdd, self.dt)
         
         # should be 1 x num_features
         state_pred_lift = self.lift_space(state_pred)
-        state_pred_lift = state_pred_lift.reshape(1,6)
+        state_pred_lift = state_pred_lift.reshape(1,6)    
 
-        # should be n_datapoints x num_features
+       # should be n_datapoints x num_features
+        error = state_meas - state_pred[1:]
+        error = error.reshape(1,5)  
+
+        return state_pred_lift, error
+
+    
+    def recursive_least_square(self, previous_state, control, state_meas):
+        """compute a recursive least square update
+
+        Args:
+            previous_state (np.array): state at timestep K-1
+            control (np.array): control at timestep K-1
+            state_meas (np.array): state measured at time K  
+
+        """
+        state_pred_lift, error = self.compute_error_and_lift(previous_state, control, state_meas)
+
+        if(self.P_least_square == None):
+            self.P_least_square = np.linalg.pinv(state_pred_lift.T@state_pred_lift)
+            self.best_param = self.P_least_square@state_pred_lift.T@error
+
+        else:
+            self.P_least_square = self.P_least_square - self.P_least_square@state_pred_lift.T@np.linalg.pinv(np.identity(6) + state_pred_lift@self.P_least_square@state_pred_lift.T)@state_pred_lift@self.P_least_square
+            K_least_square = self.P_least_square@state_pred_lift.T
+            self.best_param = self.best_param + K_least_square@(error)
+    
+    
+    def full_least_square(self, previous_state, control, state_meas):
+        """compute a full least square update
+
+        Args:
+            previous_state (np.array): state at timestep K-1
+            control (np.array): control at timestep K-1
+            state_meas (np.array): state measured at time K  
+
+        """
+        state_pred_lift, error = self.compute_error_and_lift(previous_state, control, state_meas)
+
         if(not np.any(self.phi_vec)):
             self.phi_vec = state_pred_lift
         else:
             self.phi_vec = np.append(self.phi_vec, state_pred_lift, axis=0)
 
-        # should be n_datapoints x num_features
-        error = state_meas - state_pred[1:]
-        error = error.reshape(1,5)
         if(not np.any(self.error_vec)):
             self.error_vec = error
         else:
             self.error_vec = np.append(self.error_vec, error, axis=0)
-        
-    def update_parameters(self,):
-        # least square
-        self.best_param = np.linalg.pinv(self.phi_vec.T@self.phi_vec)@self.phi_vec.T@self.error_vec
 
-        print("best_param", self.best_param)
+        self.P_least_square = np.linalg.pinv(self.phi_vec.T@self.phi_vec)
+        self.best_param = self.P_least_square@self.phi_vec.T@self.error_vec
 
-
-        
 
     def compute_control(self, state, state_des):
+        """compute feedforward and LQR control inputs
+
+        Args:
+            state (np.array): actual robot state
+            state_des (np.array): desired robot state
+
+        Returns:
+            (np.array): optimized control inputs
+
+        """
         state_des[1] = self.twip.compute_angle_from_vel(state_des[3])
         u_ff = self.twip.compute_feed_forward(state_des[1], state_des[3])
         u_ff = np.ones(2)*u_ff
@@ -108,19 +191,19 @@ class LQR:
 
 
 if __name__=="__main__":
-    control = LQR(dt=0.01)
+    control = LQR_LS(dt=0.01, horizon=2000)
     
     x = np.array([0, 0, 0, 0, 1., 0.])
     u = np.array([0.1, 0.1])
     y_meas = np.array([0, 0, 0, 0.95, 0])
 
-    control.update_dataset(x, u, y_meas)
+    control.full_least_square(x, u, y_meas)
 
     x2 = np.array([0, 0, 0, 0, 0.95, 0.])
     u2 = np.array([0.1, 0.1])
     y2_meas = np.array([0, 0, 0, 1.9, 0])
-    control.update_dataset(x2, u2, y2_meas)
+    control.recursive_least_square(x2, u2, y2_meas)
 
-    control.update_parameters()
+    
     
     
